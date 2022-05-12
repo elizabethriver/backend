8518d236592bf777bd01741165f7286d76a7f833
Elizabeth <elizabethriver@gmail.com>
2022-05-12 08:57:41 -0500

    cors

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 app.use(express.json());
-app.use(cors());
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   // res.header('Access-Control-Allow-Credentials', 'true');
